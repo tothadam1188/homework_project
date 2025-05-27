d428887c4c55b3ef7a01cbd57b55ce0b37d5d150
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-27 16:13:38 +0200

    feat(model): add Move record to represent player moves on the board

---
+++ src/main/java/hu/unideb/inf/model/Move.java
@@ -0,0 +1,9 @@
+package hu.unideb.inf.model;
+
+/**
+ * Represents a move on the game board.
+ *
+ * @param row the row index of the move
+ * @param col the column index of the move
+ */
+public record Move(int row, int col) {}
