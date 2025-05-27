8fe34513dd6afc753a68b707feef0c17ba95c94f
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-27 15:46:49 +0200

    feat:(model): create GameModel class skeleton with game.State inferface methods

---
+++ src/main/java/hu/unideb/inf/model/GameModel.java
@@ -0,0 +1,55 @@
+package hu.unideb.inf.model;
+
+import game.State;
+
+public class GameModel implements State{
+    /**
+     * @return
+     */
+    @Override
+    public Player getNextPlayer() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public boolean isGameOver() {
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Status getStatus() {
+        return null;
+    }
+
+    /**
+     * @param player
+     * @return
+     */
+    @Override
+    public boolean isWinner(Player player) {
+        return State.super.isWinner(player);
+    }
+
+    /**
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean isLegalMove(Object o) {
+        return false;
+    }
+
+    /**
+     * @param o
+     */
+    @Override
+    public void makeMove(Object o) {
+
+    }
+}
