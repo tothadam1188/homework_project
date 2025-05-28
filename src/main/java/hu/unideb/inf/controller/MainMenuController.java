8409b8ee43841376f21f26e2928bd904f477ca4a
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 16:35:29 +0200

    feat: add tinylog logging to MainMenuController

--- src/main/java/hu/unideb/inf/controller/MainMenuController.java
+++ src/main/java/hu/unideb/inf/controller/MainMenuController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.tinylog.Logger;
 
 import java.io.IOException;
 
@@ -26,15 +27,21 @@ public class MainMenuController {
      */
     @FXML
     void onStartClick(MouseEvent event) throws IOException {
+        Logger.debug("Start button clicked");
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/game_view.fxml"));
         Parent parent = loader.load();
+        Logger.debug("Game view loaded from FXML");
 
         GameController controller = loader.getController();
+
+        Logger.info("Player 1: '{}', Player 2: '{}'", player1Name.getText(), player2Name.getText());
         controller.setPlayers(player1Name.getText(), player2Name.getText());
 
         Scene scene = new Scene(parent);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+        Logger.debug("Game scene loaded");
     }
 }
