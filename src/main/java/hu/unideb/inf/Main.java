9c7e3f36ab221cf68ff77fb9344a2239c52c55f4
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 13:50:00 +0200

    chore: create Main class to launch JavaFX application

--- src/main/java/hu/unideb/inf/Main.java
+++ src/main/java/hu/unideb/inf/Main.java
@@ -1,4 +1,11 @@
 package hu.unideb.inf;
 
-public class Main {
+import hu.unideb.inf.view.GameApplication;
+import javafx.application.Application;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Application.launch(GameApplication.class, args);
+    }
 }
