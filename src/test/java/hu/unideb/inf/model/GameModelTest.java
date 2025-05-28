2c6827b429a3158991f1e49bd2c2920485d1ddea
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 20:42:06 +0200

    test(model): fixed certain tests and added tests for getBoardCell method

--- src/test/java/hu/unideb/inf/model/GameModelTest.java
+++ src/test/java/hu/unideb/inf/model/GameModelTest.java
@@ -22,22 +22,33 @@ public class GameModelTest {
 
     @Test
     void toStringMethodCorrectlyDisplaysGameBoard(){
-        assertEquals("0 1 0 1 0 1 0 1 0 1 0 \n2 0 2 0 2 0 2 0 2 0 2 \n0 1 0 1 0 1 0 1 0 1 0 \n2 0 2 0 2 0 2 0 2 0 2 \n0 1 0 1 0 1 0 1 0 1 0 \n" +
-                            "2 0 2 0 2 0 2 0 2 0 2 \n0 1 0 1 0 1 0 1 0 1 0 \n2 0 2 0 2 0 2 0 2 0 2 \n0 1 0 1 0 1 0 1 0 1 0 \n2 0 2 0 2 0 2 0 2 0 2 \n0 1 0 1 0 1 0 1 0 1 0 \n"
+        assertEquals("""
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        2 0 2 0 2 0 2 0 2 0 2\s
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        2 0 2 0 2 0 2 0 2 0 2\s
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        2 0 2 0 2 0 2 0 2 0 2\s
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        2 0 2 0 2 0 2 0 2 0 2\s
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        2 0 2 0 2 0 2 0 2 0 2\s
+                        0 1 0 1 0 1 0 1 0 1 0\s
+                        """
                             ,gameModel.toString());
     }
 
     @Test
     void gameModelClassDetectsPlayer1Win(){
         gameModel.makeMove(new Move(1,1));
-        gameModel.makeMove(new Move(3,1));
         gameModel.makeMove(new Move(1,3));
-        gameModel.makeMove(new Move(5,1));
+        gameModel.makeMove(new Move(3,1));
         gameModel.makeMove(new Move(1,5));
-        gameModel.makeMove(new Move(7,1));
+        gameModel.makeMove(new Move(5,1));
         gameModel.makeMove(new Move(1,7));
-        gameModel.makeMove(new Move(9,1));
+        gameModel.makeMove(new Move(7,1));
         gameModel.makeMove(new Move(1,9));
+        gameModel.makeMove(new Move(9,1));
 
         assertTrue(gameModel.isGameOver());
         assertEquals(State.Status.PLAYER_1_WINS, gameModel.getStatus());
@@ -49,13 +60,13 @@ public class GameModelTest {
         gameModel.makeMove(new Move(1,1));
         gameModel.makeMove(new Move(1,1));
         gameModel.makeMove(new Move(3,2));
-        gameModel.makeMove(new Move(3,1));
         gameModel.makeMove(new Move(1,3));
-        gameModel.makeMove(new Move(5,1));
+        gameModel.makeMove(new Move(3,1));
         gameModel.makeMove(new Move(1,5));
-        gameModel.makeMove(new Move(7,1));
+        gameModel.makeMove(new Move(5,1));
         gameModel.makeMove(new Move(1,7));
-        gameModel.makeMove(new Move(9,1));
+        gameModel.makeMove(new Move(7,1));
+        gameModel.makeMove(new Move(1,9));
 
         assertTrue(gameModel.isGameOver());
         assertEquals(State.Status.PLAYER_2_WINS, gameModel.getStatus());
@@ -65,6 +76,16 @@ public class GameModelTest {
     @Test
     void gameModelClassDetectsIllegalMove(){
         assertThrows(GameModelException.class, () -> gameModel.makeMove(new Move(1,11)));
+    }
+
+    @Test
+    void getBoardCellDetectsIllegalMove(){
+        assertThrows(GameModelException.class, () -> gameModel.getBoardCell(1,11));
+    }
+
+    @Test
+    void getBoardCellGivesCorrectValue(){
+        assertEquals(1, gameModel.getBoardCell(0,1));
     }
 
 }
