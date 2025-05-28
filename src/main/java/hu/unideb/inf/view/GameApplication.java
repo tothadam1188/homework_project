af14e82807c15709e9e34424a84d7123f0bedbb8
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 13:49:23 +0200

    feat(view): add JavaFX application entry point to load main menu

---
+++ src/main/java/hu/unideb/inf/view/GameApplication.java
@@ -0,0 +1,21 @@
+package hu.unideb.inf.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GameApplication extends Application{
+
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main_menu.fxml"));
+        stage.setTitle("JavaFX Board Game Example");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
