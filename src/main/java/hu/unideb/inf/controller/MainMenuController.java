da5cc06921b359e22bd2abfae74d496df67d4066
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 13:24:21 +0200

    feat(controller): add MainMenuController skeleton

---
+++ src/main/java/hu/unideb/inf/controller/MainMenuController.java
@@ -0,0 +1,15 @@
+package hu.unideb.inf.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MainMenuController {
+    @FXML private TextField player1Name;
+    @FXML private TextField player2Name;
+
+    @FXML
+    void onStartClick(MouseEvent event){
+
+    }
+}
