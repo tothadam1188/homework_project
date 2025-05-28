8bb2b01201524b4171a61ae675b36e01b1ffc0e6
Tóth Ádám <toth.adam@mailbox.unideb.hu>
2025-05-28 12:15:25 +0200

    test(model): add unit tests for ConsoleGame

---
+++ src/test/java/hu/unideb/inf/model/ConsoleGameTest.java
@@ -0,0 +1,21 @@
+package hu.unideb.inf.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConsoleGameTest {
+    @Test
+    void parseMoveValidInputReturnsMove(){
+        var move = ConsoleGame.parseMove("3 4");
+        assertEquals(3, move.row());
+        assertEquals(4, move.col());
+    }
+
+    @Test
+    void parseMoveInvalidInputsThrowException() {
+        assertThrows(ConsoleGameException.class, () -> ConsoleGame.parseMove("a"));
+        assertThrows(ConsoleGameException.class, () -> ConsoleGame.parseMove(""));
+        assertThrows(ConsoleGameException.class, () -> ConsoleGame.parseMove("1"));
+        assertThrows(ConsoleGameException.class, () -> ConsoleGame.parseMove("1 1 1"));
+    }
+}
